30b2113958427d480ab8d7e84b3271a6e3df028e
MaggieXuzy <maggiexuzy@163.com>
2023-08-22 16:43:18 +0800

    Modify IntList.java dcatenate(2)

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,7 +81,7 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        if (A == null) return null;
+        if (A == null) return B;
         IntList C = A;
         while (A.rest != null){
             A = A.rest;
